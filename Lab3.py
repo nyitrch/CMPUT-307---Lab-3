d800ae61153e7b763e35d2fd07de8f81cb6a7ac1
nyitrch <c.nyitrai@gmail.com>
2016-10-01 08:44:04 -0600

    New changes!
    I realized our method of passing an identity matrix through the functions to get out the transformation matrix was not working as we wanted. For example, the matrix actually used in a translation is of a 4x4 format, and the one we got out was a 3x3. Now each of the scale, translation, and rotation functions return a transformed object and the matrix that was used to do the transformation. I standardized our variable names, function names, comment style, and whitespace style.

--- Lab3.py
+++ Lab3.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 CMPUT 307 Fall 2016 
 LEC A1 LAB D01
@@ -12,45 +11,35 @@ Michael Xi
 import numpy as np
 import math
 
-# Matrix transformation functions.
+# Throughout this file 'obj' is taken to mean a list of lists, with each
+# nested list containing the 3D coordinates of a vertex [x,y,z]. For example,
+# in shrink_obj() obj takes this meaning, NOT the meaning of a .obj file.
+# obj does not mean a .obj file unless specified.
 
-# Expand and shirnk are used to navigate between matrices that contain
-# coordinates we want to write to and from files, and ones we want to
-# perform transformations on.
-def expand_mat(mat):
-    # This function converts a 3D matrix to a 4D version 
-    # so that it is compatible with homogeneous transforms.
-    for coordinate in mat:
-        coordinate.append(1)
+# Matrix and obj alteration functions.
+def expand_trans_mat(trans_mat):
+    # This function expands a given transformation matrix to be compatible
+    # with homogenous transforms.
+    for i in trans_mat:
+        i.append(0)
+    trans_mat.append([0,0,0,1])
     return
 
-def shrink_mat(mat):
-    # This function does the opposite of expand_mat, returning
-    # a 3D matrix converted from a 4D matrix back to its 3D status.
-    for coordinate in mat:
-        coordinate.pop()
-    return
-
-# Function for rotating object around y-axis.
-def rota_y(degree,obj):
-    rotated_obj = []
-    degree = math.radians(degree)
-    sr=np.sin(degree)
-    cr=np.cos(degree)
-    rota_Mat=[[cr,0,sr],[0,1,0],[-sr,0,cr]]
+def expand_obj(obj):
+    # This function converts a matrix of 3D vertices to one compatible with
+    # homogeneous transforms.
     for vertex in obj:
-        rotated_obj.append(np.dot(rota_Mat, vertex).tolist())
-
-    return rotated_obj
-
-def writeMatrixToFile(matrix):
-    matrix=str(matrix)
-    with open('Transformation matrixes.txt','a') as target:
-        target.write(matrix)
-    target.close()
+        vertex.append(1)
     return
 
-# The scale, translate, and rotation functions all take only 3x3 matrices.
+def shrink_obj(obj):
+    # This function does the opposite of expand_obj, returning
+    # a expanded 3D vertex matrix back to only having 3 coordinates.
+    for vertex in obj:
+        vertex.pop()
+    return
+
+# The scale, rotation, and translation functions we use on an object.
 def obj_scale(obj,x_scale,y_scale,z_scale):
     # Takes a 3D obj matrix and returns a version of it scaled by
     # the scale value for each coordinate.
@@ -64,12 +53,12 @@ def obj_scale(obj,x_scale,y_scale,z_scale):
         # matrix, then add it to our new scaled version of the original obj.
         # .tolist() is needed to keep our types consistent.
         scaled_obj.append(np.dot(scale_mat,vertex).tolist())
-    return scaled_obj
+    return scaled_obj, scale_mat
 
 def obj_trans(obj, trans_x, trans_y, trans_z):
     # Translates the vertices in a 3D obj matrix and returns the translated
     # matrix.
-    expand_mat(obj)
+    expand_obj(obj)
     trans_mat = [[1,0,0,trans_x],
                  [0,1,0,trans_y],
                  [0,0,1,trans_z],
@@ -82,59 +71,106 @@ def obj_trans(obj, trans_x, trans_y, trans_z):
         # of the original obj. tolist() is needed again to keep our types
         # consistent.
         trans_obj.append(np.dot(trans_mat,vertex).tolist())
-    shrink_mat(obj)
-    shrink_mat(trans_obj)
-    return trans_obj
+    shrink_obj(obj)
+    shrink_obj(trans_obj)
+    return trans_obj, trans_mat
+    
+def rot_obj_y(obj,degree):
+    # Function for rotating an object around y-axis by degree 'degree'.
+
+    # Convertion of the given degree for compatibility with the
+    # .sin and .cos functions.
+    degree = math.radians(degree)
+    
+    # Creation of the rotation matrix.
+    sr = math.sin(degree)
+    cr = math.cos(degree)
+    rota_mat = [[cr,0,sr],[0,1,0],[-sr,0,cr]]
+    
+    # Iterate through the obj passed in and transform each vertex.
+    rotated_obj = []
+    for vertex in obj:
+        # Similarly to the other transformation functions, we rotate
+        # each vertex using matrix multiplication with a rotation matrix.
+        # The new vertex is then added to a new matrix as a list.
+        rotated_obj.append(np.dot(rota_mat, vertex).tolist())
+    return rotated_obj, rota_mat
+
+
+def write_matrixes_to_file(mat1,mat2,mat3):
+    # Function used for writing the transformation matrixes to the file.
+
+    # Conversion of matrixes to format which can be written easily.
+    mat1, mat2, mat3 = str(mat1), str(mat2), str(mat3)
+    
+    # Writing of matrixes to file.
+    with open('Transformation matrixes.txt','a') as target:
+        target.write('Matrix for transformation 1: %s\n' % mat1)
+        target.write('Matrixes for transformation 2: %s\n' % mat2)
+        target.write('Matrixes for transformation 3: %s' % mat3)
+    target.close()
+    return
+
 
 # Actual transformations as specified by the assignment.
-#Transform 1
-def trans1(objs,identity):
-    obj4 = obj_scale(objs[0], 4, 4, 4)
+# Transform 1: Scale the vertices of the cube by 4.
+def trans1(objs):
+    # Scaling by 4.
+    obj4, trans1_scale_mat = obj_scale(objs[0],4,4,4)
+    
+    # Writing to file.
     target = open('O4.obj', 'w')
     for i in obj4:
-        target.write('v %s %s %s\n' % (i[0], i[1], i[2]))
+        target.write('v %s %s %s\n' % (i[0],i[1],i[2]))
     target.close()
 
-    # Transformation 1 Matrix Pull
-    trans1_mat = obj_scale(identity, 4, 4, 4)
-    return trans1_mat
+    # Transformation 1 matrix pull.
+    return trans1_scale_mat
 
-#Transform 2
-def trans2(objs,identity):
-    obj5_trans = obj_trans(objs[1],0,100,0)
-    obj5 = obj_scale(obj5_trans, 5,5,5)
+# Transform 2:
+# Translate the cylinder by 100 units in y axis and scale it by 5 units.
+def trans2(objs):
+    # Translation of 100 in y.
+    obj5_trans, trans2_trans_mat = obj_trans(objs[1],0,100,0)
+    # Scaling by 5.
+    obj5, trans2_scale_mat = obj_scale(obj5_trans, 5,5,5)
+    
+    # Writing to file.
     target = open('O5.obj','w')
     for i in obj5:
         target.write('v %s %s %s\n'% (i[0],i[1],i[2]))
     target.close()
 
-    #Transformation 2 Matrix Pull
-    trans2_mat=obj_trans(identity,0,100,0)
-    trans2_mat=obj_scale(trans2_mat,5,5,5)
-    return trans2_mat
+    #Transformation 2 matrix pull.
+    expand_trans_mat(trans2_scale_mat)
+    return trans2_trans_mat, trans2_scale_mat
 
 # Transform 3
-def trans3(objs,identity):
-    obj6_rot = rota_y(90,objs[2])
-    obj6_trans = obj_trans(obj6_rot,0,0,10)
-    obj6_scale = obj_scale(obj6_trans,3,3,3)
+def trans3(objs):
+    # Rotation about y.
+    obj6_rot, trans3_rot_mat = rot_obj_y(objs[2],90)
+    # Translation by 10 in z.
+    obj6_trans, trans3_trans_mat = obj_trans(obj6_rot,0,0,10)
+    # Scaling by 3.
+    obj6, trans3_scale_mat = obj_scale(obj6_trans,3,3,3)
+    
+    # Writing to file.
     target = open('O6.obj','w')
-    for i in obj6_scale:
+    for i in obj6:
         target.write('v %s %s %s\n'% (i[0],i[1],i[2]))
     target.close()
 
-    #Transformation 3 Matrix Pull
-    trans3_mat=rota_y(90, identity)
-    trans3_mat=obj_trans(trans3_mat,0,0,10)
-    trans3_mat=obj_scale(trans3_mat,3,3,3)
-    return trans3_mat
+    #Transformation 3 matrix pull.
+    expand_trans_mat(trans3_rot_mat)
+    expand_trans_mat(trans3_scale_mat)
+    return trans3_rot_mat, trans3_trans_mat, trans3_scale_mat
 
 def main():
     # Initialization of array of .objs we will read and the object counter.
-    objs = [[], [], []]
+    objs = [[],[],[]]
     obj_count = 0
-
-    # Opening the given .obj
+    
+    # Opening the given .obj file.
     with open('Mixed.obj') as f:
         bigdata = f.readlines()
 
@@ -145,7 +181,7 @@ def main():
         if not splitline:
             continue
 
-            # If the first continuous set of characters on the line is 'v',
+        # If the first continuous set of characters on the line is 'v',
         # we store the coordinates in our objs array.
         if splitline[0] == 'v':
             x = float(splitline[1])
@@ -158,13 +194,14 @@ def main():
         elif splitline[0] == 'g':
             obj_count += 1
 
-    identity = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    trans1_mat=trans1(objs,identity)
-    trans2_mat=trans2(objs,identity)
-    trans3_mat=trans3(objs,identity)
-    writeMatrixToFile(trans1_mat)
-    writeMatrixToFile(trans2_mat)
-    writeMatrixToFile(trans3_mat)
+    # The idea is that by feeding the trans1,2,3 functions the identity matrix
+    # they return the transformation matrix used.
+    trans1_mat = trans1(objs)
+    trans2_mat = trans2(objs)
+    trans3_mat = trans3(objs)
+
+    # Now we write the matrixes to a seperate file.
+    write_matrixes_to_file(trans1_mat,trans2_mat,trans3_mat)
 
 main()
 
